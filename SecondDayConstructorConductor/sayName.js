af25fc140eab535a5a2d98c0a319b3331672d54a
Jon <jon@allen8r.com>
2015-01-29 13:47:52 -0700

    finish second task in assignment repo

--- SecondDayConstructorConductor/sayName.js
+++ SecondDayConstructorConductor/sayName.js
@@ -1,13 +1,25 @@
 //Create a Person constructor that accepts name and age as parameters and sets those properties accordingly in the Constructor.
 
   //code here
-
+var Person = function(name, age) {
+    this.name = name;
+    this.age = age;
+};
 
 //Now create three instances of Person with data you make up
 
   //code here
-
-
+var user1 = new Person('Jon', '50');
+var user2 = new Person('Tom', '20');
+var user3 = new Person('Alex', '15');
 //Now add a sayName method on your Person class that will alert the name of whatever Person instance called it.
 
   //code here
+Person.prototype.sayName = function(){
+  alert(this.name);  
+};
+
+
+user1.sayName();
+user2.sayName();
+user3.sayName();
